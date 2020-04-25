27283c9900dffa76ed2a709eb8112a6adfe3adf1
Matheus Santos <matheusmendes1@hotmail.com>
2020-04-25 17:04:50 -0300

    SignIn Page Checkpoint

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,17 +1,81 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
-import { Container, Title } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Title>Do your logon</Title>
-    </Container>
+            <View>
+              <Title>Do your logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Password" />
+
+            <Button
+              onPress={() => {
+                // eslint-disable-next-line no-console
+                console.log('Pressionou');
+              }}
+            >
+              Enter
+            </Button>
+
+            <ForgotPassword
+              onPress={() => {
+                // eslint-disable-next-line no-console
+                console.log('Pressionou');
+              }}
+            >
+              <ForgotPasswordText>Forgot my password</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton
+        onPress={() => {
+          // eslint-disable-next-line no-console
+          console.log('Pressionou');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create an account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
